8990d4e8445acec74d460a19af553b94e5aa41cc
Diksha <makkardiksha02@gmail.com>
2024-07-09 00:09:07 +0530

    Create Article.java

---
+++ src/main/java/com/example/NewsApp/model/Article.java
@@ -0,0 +1,40 @@
+package com.example.NewsApp.model;
+
+public class Article {
+    private String articleId;
+    private String articleType;
+    private String articleTitle;
+    private String articleUrl;
+
+    public String getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(String articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getArticleType() {
+        return articleType;
+    }
+
+    public void setArticleType(String articleType) {
+        this.articleType = articleType;
+    }
+
+    public String getArticleTitle() {
+        return articleTitle;
+    }
+
+    public void setArticleTitle(String articleTitle) {
+        this.articleTitle = articleTitle;
+    }
+
+    public String getArticleUrl() {
+        return articleUrl;
+    }
+
+    public void setArticleUrl(String articleUrl) {
+        this.articleUrl = articleUrl;
+    }
+}
